525604f697808484d5082a973b996e3a6a2ec5b3
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-08-26 16:04:32 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import VideoHero from './components/VideoHero'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <VideoHero /> */}
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Configure your color palette here
+         'custom-purple':'#548ac8',
+        },
+    },
+  },
+  plugins: [],
+}
+
